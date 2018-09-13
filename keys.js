51d2b37033ddcaab16b36cb3c9fe32e67117e790
Irene R <irene@smallwondermedia.com>
2018-09-12 22:03:37 -0400

    3

--- keys.js
+++ keys.js
@@ -1,7 +1,7 @@
 console.log('this is loaded');
 
 exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
+  id: process.env.b1961150dc3e42ac940cc7452b7b2c72,
+  secret: process.env.Saba6bee8af644192874f529f6ed45f46
 };
 
--- liri.js
+++ liri.js
@@ -4,9 +4,28 @@ var fs = require("fs");
 var moment = require("moment");
 // moment().format();
 var http = require("http");
-var spotify = require("node-spotify-api");
+var Spotify = require("node-spotify-api");
 var request = require("request");
-
+var express = require("express");
 var keys = require("/keys.js");
 
+var PORT = 8080;
+
+// ALL CONSOLE LOG
+
+// spotify this song
+
 var spotify = new Spotify(keys.spotify);
+
+
+// function spotifyRequest for song
+// get response from Spotify API
+// relevant API endpoints
+// ex:  spotify:track:6rqhFgbbKwnb9MLmUQDhG6
+
+// 1. identify song; var song = GET https://api.spotify.com/v1/tracks/{id}
+
+// 2. identify artist; var artist = GET https://api.spotify.com/v1/artists/{id}
+// 3 . preview link; tracks/preview_url
+// 4. identify album; var album = GET https://api.spotify.com/v1/albums/{id}
+// 5. no song input = The Sign by Ace of Base
