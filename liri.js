7f28210e89315ea69704916302c6d17b12e6d701
Irene R <irene@smallwondermedia.com>
2018-09-12 20:34:26 -0400

    1

--- liri.js
+++ liri.js
@@ -1,1 +1,11 @@
 require("dotenv").config();
+var fs = require("fs");
+var moment = require("moment");
+// moment().format();
+var http = require("http");
+var spotify = require("node-spotify-api");
+var req = require("request");
+
+var keys = require("/keys.js");
+
+var spotify = new Spotify(keys.spotify);
