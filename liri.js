a130b85ecafe2fdbc25b1d548c2184d6adf11129
Irene R <irene@smallwondermedia.com>
2018-09-13 23:07:37 -0400

    omg 3 of 4 things work

--- liri.js
+++ liri.js
@@ -4,103 +4,91 @@ var fs = require("fs");
 var moment = require("moment");
 // moment().format();
 var http = require("http");
-// var Spotify = require("node-spotify-api");
 var request = require("request");
 var express = require("express");
 var bodyParser = require("body-parser");
 var keys = require("./keys.js");
 
-// Sets up the Express App
-var app = express();
-var PORT = 8080;
 
-// Sets up the Express app to handle data parsing
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+// SPOTIFY
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
+
+var searchSpotify = process.argv[3];
+  // when empty
+  if (searchSpotify === undefined) {
+    searchSpotify = "The Sign";
+  };
+ 
+// function spotifyThis(search);
+function spotifyThis(song) {
+spotify
+  .search({ type: 'track', query: song })
+  .then(function(response) {
+    // identify artist
+    if (song === "The Sign") {
+      console.log(response.tracks.items[7].album.artists[0].name)
+          // identify song
+      console.log(response.tracks.items[7].name);
+          // preview link
+      console.log(response.tracks.items[7].preview_url);
+      // identify album
+      console.log(response.tracks.items[7].album.name);
+    }
+    else {
+      console.log("Artist: " + response.tracks.items[0].album.artists[0].name)
+        // identify song
+      console.log("Song: " + response.tracks.items[0].name);
+          // preview link
+      console.log("Audio preview: " + response.tracks.items[0].preview_url);
+      // identify album
+      console.log("Album: " + response.tracks.items[0].album.name);
+  }
+  })
+  .catch(function(err) {
+    // no song input
+    console.log(err);
+  });
+};  //end function spotifyThis()
 
 
-// ALL CONSOLE LOG
+// OMDB
+var searchOMDB = process.argv[3];
+  // when empty
+  if (searchOMDB === undefined) {
+    searchOMDB = "Mr. Nobody";
+  };
+  var movieName = "";
 
-// BANDS IN TOWN API
-function concertThis() {
-    var artist = madonna;
+// function movieThis()
+function movieThis(searchOMDB) {
+  for (var i = 2; i < searchOMDB.length; i++) {
 
+    if (i > 2 && i < searchOMDB.length) {
+  
+      movieName = movieName + "+" + searchOMDB[i];
+  
+    }
+  
+    else {
+  
+      movieName += searchOMDB[i];
+  
+    }
+  }
 
-    app.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp", function(req, res) {
-        return res.json(artists);
-    });
-    // name of venue
-    // venue location
-    // date of event ("MM/DD/YYYY")
-
-
-
-};
-
-
-// Starts the server to begin listening
-// =============================================================
-app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+  var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+  console.log(queryUrl);
+  request(queryUrl, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      console.log("Release Year: " + JSON.parse(body).Year);
+    }
   });
 
 
 
+}; //end function movieThis()
 
-
-
-
-
-
-
-
-
-
-// spotify this song
-
-// var spotify = new Spotify(keys.spotify);
-
-
-// function spotifyRequest for song
-// get response from Spotify API
-// relevant API endpoints
-// ex:  spotify:track:6rqhFgbbKwnb9MLmUQDhG6
-
-// function spotifyRequest(req, res) {
-//     var spotify = new Spotify({
-//         id: b1961150dc3e42ac940cc7452b7b2c72,
-//         secret: Saba6bee8af644192874f529f6ed45f46
-//     });
-//     spotify
-//   .request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx')
-//   .then(function(data) {
-//     console.log(data); 
-//   })
-//   .catch(function(err) {
-//     console.error('Error occurred: ' + err); 
-//   });
-// }; //end function spotifyRequest()
-
-
-
-// 1. identify song; var song = GET https://api.spotify.com/v1/tracks/{id}
-
-// 2. identify artist; var artist = GET https://api.spotify.com/v1/artists/{id}
-// 3 . preview link; tracks/preview_url
-// 4. identify album; var album = GET https://api.spotify.com/v1/albums/{id}
-// 5. no song input = The Sign by Ace of Base
-
-
-function movieInfo(req, res) {
-    // var userMovie = 
-
-    // http://www.omdbapi.com/?apikey=d9666985&
-
-    request('http://www.google.com', function (error, response, body) {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
-    });
 // title of movie
 // year of release
 // imdb rating
@@ -110,10 +98,45 @@ function movieInfo(req, res) {
 // plot
 // actors
 
-// no name = Mr. Nobody
 
 
 
-}; // end function movieInfo()
+// spotify-doWhat
+function doWhat() {
+  // connect to random.txt
+  fs.readFile("./random.txt", function(err, data) {
+    // console.log(data.toString());
+    // take text and turn it into search
+    var randomTxt = data.toString().split(",");
+    console.log(randomTxt);
+    // run spotifyThis()
+    spotifyThis(randomTxt[1]);
+  }); 
+};
 
 
+
+
+
+// Bands in Town
+    // name of venue
+    // venue location
+    // date of event ("MM/DD/YYYY")
+
+
+
+
+
+
+
+
+switch(process.argv[2]) {
+  case "spotify-this-song": spotifyThis(searchSpotify);
+  break; // break = skip rest of switch statement
+  case "movie-this": movieThis(searchOMDB);
+  break; 
+  case "do-what-it-says": doWhat(process.argv[3]);
+  break; 
+  case "concert-this": concertThis(process.argv[3]);
+  break; 
+};
